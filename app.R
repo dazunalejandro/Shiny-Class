a0b0601ea71d86f950ca19010a402cb8d4aa2f05
Alejandro Dazun <dazunalejandro@gmail.com>
2019-03-06 10:50:10 +0100

    Server Change

--- app.R
+++ app.R
@@ -21,23 +21,31 @@ ui <- fluidPage(
    
    includeMarkdown("references.md"),
    
-   selectInput("select", label = h3("Plot by type of alimentation"), 
-               choices = list_choices,
-               selected = 1),
+   selectInput("select", label = h3("Plot by type of alimentation"),
+               choices=character(0),selected = 1),
    
    plotOutput(outputId = "plot")
 )
 
-col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
+
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  output$plot <- renderPlot({
-    ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +
-      col_scale +
-      geom_point() 
-  })
+server <- function(input, output,session){
+  col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
+  
+  updateSelectInput(session, "select",
+                    choices = list_choices,
+                    selected = tail(list_choices, 1))
+  
+
+    output$plot <- renderPlot({
+      if(input$select != ""){
+        ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+          scale_x_log10() +
+          col_scale +
+          geom_point()
+      }
+    })
    
 }
 
